94fd35a6f2d52700090dd2a58e14f80ef654ed3a
Nagasena <ngsn4cr@gmail.com>
2026-04-11 20:33:45 +0900

    GithubAction-Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://na-gasena.github.io',
+  base: '/PEAK_END_WEB2026',
   vite: {
     plugins: [tailwindcss()],
   },
